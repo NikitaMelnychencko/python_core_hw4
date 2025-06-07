7cee015afeb293014f0cca4cfb16c32b5d5f3615
Nikita Melnychenсko <82110006+NikitaMelnychencko@users.noreply.github.com>
2025-06-07 19:04:28 +0300

    Make task 2

---
+++ src/task_2/task_2.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+def get_cats_info(path):
+    cats_info = []
+    try:
+        with open(path, 'r' , encoding='utf-8') as file:
+            for line in file:
+                uuid, name, age = line.split(',')
+                formatted_age = None
+                try:
+                    formatted_age = int(age)
+                except ValueError:
+                    print(f"{age} is not a number")
+                cats_info.append({
+                    'id': uuid,
+                    'name': name,
+                    'age': formatted_age
+                })
+    except FileNotFoundError:
+        print("File not found")
+        return []
+
+    return cats_info
+
+
+file_name = Path(__file__).parent / 'cats.txt'
+
+result = get_cats_info(file_name)
+
+print(result)
